791d93048a83bad406cbead95cf681d0ba3c0e22
Miriam Schnoll <miriamschnoll1996@gmail.com>
2019-03-10 18:40:45 -0400

    add actions to buttons of LegueVC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    @IBAction func onWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "womens")
+    }
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+       selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType:String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     @IBAction func onNextTapped(_ sender: Any) {
         
         
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+       
         
     }
     
@@ -24,6 +41,7 @@ class LeagueVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Test on 3/10/19.
+//  Copyright © 2019 miriam schnoll. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague:String?
+    var sslectedSkillLevel: String?
+}
